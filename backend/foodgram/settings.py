0255237f45263d171bbc038dd3ec89ddde16739b
Кирилл Шутов <shutovka14@yandex.ru>
2023-04-11 13:15:28 +0300

    006

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='UanMIW18K')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['51.250.89.101', '127.0.0.1', 'localhost']
 
 
 # Application definition
